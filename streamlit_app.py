b7fe35ce07cde116e829dba8dc28ed3c72f9334c
victorolivod <133779691+victorolivod@users.noreply.github.com>
2025-05-27 18:19:53 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.header('st.button')
-if st.button('Say hello'): st.write('Why hello there') else: st.write('Goodbye')
-st.write('Why hello there')
-st.write('Goodbye')
+if st.button('Say hello'):
+  st.write('Why hello there') 
+else: 
+  st.write('Goodbye')
